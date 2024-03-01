83a799c3113ca6ebefedb2315782c24d51a6358e
John Murray <jwmu@amazon.com>
2024-03-01 10:04:20 -0700

    catr initial commit

---
+++ src/main.rs
@@ -0,0 +1,86 @@
+use std::fs::File;
+use std::io::{self, BufRead, BufReader};
+
+use anyhow::Result;
+use clap::{Arg, ArgAction, Command};
+
+#[derive(Debug)]
+struct Args {
+    files: Vec<String>,
+    number_lines: bool,
+    number_nonblank_lines: bool,
+}
+
+fn main() {
+    if let Err(_e) = run(get_args()) {}
+}
+
+fn get_args() -> Args {
+    let matches = Command::new("catr")
+        .version("0.1.0")
+        .author("john@gardenway.org")
+        .about("Rust version of `cat`")
+        .arg(
+            Arg::new("files")
+                .value_name("FILE")
+                .help("Input file(s)")
+                .num_args(1..)
+                .default_value("-"),
+        )
+        .arg(
+            Arg::new("number")
+                .short('n')
+                .long("number")
+                .help("Number of lines")
+                .action(ArgAction::SetTrue)
+                .conflicts_with("number_nonblank"),
+        )
+        .arg(
+            Arg::new("number_nonblank")
+                .short('b')
+                .long("number-nonblank")
+                .help("Number non-blank lines")
+                .action(ArgAction::SetTrue),
+        )
+        .get_matches();
+
+    Args {
+        files: matches.get_many("files").unwrap().cloned().collect(),
+        number_lines: matches.get_flag("number"),
+        number_nonblank_lines: matches.get_flag("number_nonblank"),
+    }
+}
+
+fn run(args: Args) -> Result<()> {
+    for filename in args.files {
+        match open(&filename) {
+            Err(e) => eprint!("{filename}: {e}"),
+            Ok(file) => {
+                let mut prev_num = 0;
+                for (line_num, line) in file.lines().enumerate() {
+                    let line = line?;
+                    if args.number_lines {
+                        println!("{:6}\t{line}", line_num + 1);
+                    } else if args.number_nonblank_lines {
+                        if line.is_empty() {
+                            println!();
+                        } else {
+                            prev_num += 1;
+                            println!("{prev_num:6}\t{line}");
+                        }
+                    } else {
+                        println!("{line}");
+                    }
+                }
+            }
+        }
+    }
+    Ok(())
+}
+
+fn open(filename: &str) -> Result<Box<dyn BufRead>> {
+    match filename {
+        "-" => Ok(Box::new(BufReader::new(io::stdin()))),
+        _ => Ok(Box::new(BufReader::new(File::open(filename)?))),
+    }
+}
